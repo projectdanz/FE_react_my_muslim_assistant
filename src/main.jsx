7b7e49c3d657e1c730adf73c60d9cb56e92eb930
projectdanz <akunprojectdanz@gmail.com>
2025-10-18 20:18:37 +0700

    chore: setup route

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Route } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import PrayerTimeSection from './page/PrayerTimeSection'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  </StrictMode>,
+   <BrowserRouter>
+      <Routes>
+          <Route path="/prayer-time" element={<PrayerTimeSection />} />        
+      </Routes>
+    </BrowserRouter>
 )

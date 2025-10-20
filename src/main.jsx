b771df87c43f0c267fb915d2c796ff67c42bd570
projectdanz <akunprojectdanz@gmail.com>
2025-10-20 14:29:18 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import QuranSection from "./page/QuranSection.jsx";
+import ShowSurah from "./page/ShowSurah.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<QuranSection />} />
+        <Route path="/surah/:id" element={<ShowSurah />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/page/QuranSection.jsx
@@ -0,0 +1,11 @@
+import QuranSurah from '../components/QuranSurah.jsx';
+
+const QuranSection = () => {
+  return (
+    <div>
+      <QuranSurah />
+    </div>
+  );
+}
+
+export default QuranSection;

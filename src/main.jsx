66368ff4324977a339a1eb86dfaad61604cf9431
projectdanz <akunprojectdanz@gmail.com>
2025-11-06 05:06:37 +0700

    chore: replaced for page/ShowSurah to page/Surah

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QuranSection from "./page/QuranSection.jsx";
-import ShowSurah from "./page/ShowSurah.jsx";
+import Surah from "./page/Surah.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<QuranSection />} />
-          <Route path="/surah/:id" element={<ShowSurah />} />
+          <Route path="/surah/:id" element={<Surah />} />
         </Routes>
       </BrowserRouter>
   </StrictMode>
---
+++ src/page/Surah.jsx
@@ -0,0 +1,12 @@
+import ShowSurah from "../components/ShowSurah";
+
+const Surah = () => {
+
+    return(
+        <>
+            <ShowSurah />
+        </>
+    );
+}
+
+export default Surah
